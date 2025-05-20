6bae267cc018d317f6eb1d1518c38e31d11412b2
GustavoPaterno <Gustavopaterno1@gmail.com>
2025-05-19 21:32:55 -0300

    teste_mongo.v2

--- teste_mongo.py
+++ teste_mongo.py
@@ -1,16 +1,19 @@
 from pymongo import MongoClient
 from flask import Flask, jsonify, request
 
-uri = "mongodb+srv://gustavopaterno1:E910vzSF1OYgJI8I@jpg2.oaxxecf.mongodb.net/?retryWrites=true&w=majority&appName=JPG2"
+uri = "mongodb+srv://gustavopaterno1:E910vzSF1OYgJI8I@jpg2.oaxxecf.mongodb.net/?retryWrites=true&w=majority&tls=true"
+
 client = MongoClient(uri)
 db = client["jpg2"]  
-colecao = db["Jogadores"]  
+jogadores = db["Jogadores"]  
+fases = db["Fases"]
+npc = db["NPCs"]
 
 app = Flask(__name__)
 
 @app.route('/api/get/aluno', methods=['GET'])
 def get_alunos():
-    alunos = list(colecao.find({}, {"_id": 0}))  
+    alunos = list(jogadores.find({}, {"_id": 0}))  
     return jsonify(alunos)
 
 @app.route('/api/post/aluno', methods=['POST'])
@@ -27,36 +30,36 @@ def post_aluno():
     if not username or not password or not character or not progresso:
         return jsonify({"erro": "Faltando dados obrigatórios"}), 400
 
-    novo_usuario = {
+    new_usuario = {
         "username": username,
         "password": password,
         "character": character,
         "progresso": progresso
     }
 
-    colecao.insert_one(novo_usuario)
+    jogadores.insert_one(new_usuario)
     return jsonify({"mensagem": "Usuário criado com sucesso!"}), 201
 
 @app.route('/api/update/aluno', methods=['PUT'])
 def update_aluno():
     data = request.json
     username = data.get("username")  # critério de busca
-    novo_character = data.get("character")
-    novo_progresso = data.get("progresso")
+    new_character = data.get("character")
+    new_progresso = data.get("progresso")
 
     if not username:
         return jsonify({"message": "Username é obrigatório"}), 400
 
     update_fields = {}
-    if novo_character:
-        update_fields["character"] = novo_character
-    if novo_progresso:
-        update_fields["progresso"] = novo_progresso
+    if new_character:
+        update_fields["character"] = new_character
+    if new_progresso:
+        update_fields["progresso"] = new_progresso
 
     if not update_fields:
         return jsonify({"message": "Nada para atualizar"}), 400
 
-    resultado = colecao.update_one(
+    resultado = jogadores.update_one(
         {"username": username},
         {"$set": update_fields}
     )
@@ -74,7 +77,7 @@ def delete_aluno():
     if not username:
         return jsonify({"message": "info error"})
     
-    resultado = colecao.delete_one(
+    resultado = jogadores.delete_one(
         {"username": username}
     )
 
@@ -83,6 +86,170 @@ def delete_aluno():
     else:
         return jsonify({"mensagem": "Nenhum aluno deletado. Verifique o nome."})
 
+@app.route('/api/get/fases', methods=['GET'])
+def get_fases():
+    alunos = list(fases.find({}, {"_id": 0}))  
+    return jsonify(alunos)
+
+@app.route('/api/post/fases', methods=['POST'])
+def post_fases():
+    data = request.json
+    if not data:
+        return jsonify({"erro": "Dados inválidos"}), 400
+
+    name = data.get("name")
+    level = data.get("level")
+    description = data.get("description")
+
+    if not name or not description or not level:
+        return jsonify({"erro": "Faltando dados obrigatórios"}), 400
+
+    new_fase = {
+        "name": name,
+        "description": description,
+        "level": level
+    }
+
+    fases.insert_one(new_fase)
+    return jsonify({"mensagem": "fase criada com sucesso!"}), 201
+
+@app.route('/api/update/fases', methods=['PUT'])
+def update_fases():
+    data = request.json
+    level = data.get("level")  # critério de busca
+    new_name = data.get("new_name")
+    new_description = data.get("new_description")
+    new_level = data.get("new_level")
+
+    if not level or not new_name or not new_description or not new_level:
+        return jsonify({"message": "Username é obrigatório"}), 400
+
+    update_fields = {}
+    if new_name:
+        update_fields["name"] = new_name
+    if new_description:
+        update_fields["description"] = new_description
+    if new_level:
+        update_fields["level"] = new_level
+
+    if not update_fields:
+        return jsonify({"message": "Nada para atualizar"}), 400
+
+    resultado = fases.update_one(
+        {"level": level},
+        {"$set": update_fields}
+    )
+
+    if resultado.modified_count == 1:
+        return jsonify({"mensagem": "Dados da fase atualizadas com sucesso!"})
+    else:
+        return jsonify({"mensagem": "Nenhuma fase atualizada. Verifique o username."})
+
+@app.route('/api/delete/fases', methods=['DELETE'])
+def delete_fases():
+    data = request.json
+    level = data.get("level")
+    username = data.get("username")
+
+    if not level:
+        return jsonify({"message": "info error"})
+    
+    resultado = fases.delete_one(
+        {"level": level}
+    )
+
+    if resultado.deleted_count == 1:
+        return jsonify({"mensagem": "fase deletada com sucesso!"})
+    else:
+        return jsonify({"mensagem": "Nenhuma fase deletada. Verifique os campos."})
+    
+
+#NPCs
+@app.route('/api/get/npc', methods=['GET'])
+def get_npc():
+    alunos = list(npc.find({}, {"_id": 0}))  
+    return jsonify(alunos)
+
+@app.route('/api/post/npc', methods=['POST'])
+def post_npc():
+    data = request.json
+    if not data:
+        return jsonify({"erro": "Dados inválidos"}), 400
+    
+    name = data.get("name")
+    age = data.get("age")
+    gender = data.get("gender")
+    position = data.get("position")
+    profession = data.get("profession")
+
+    if not name or not age or not gender or not position or not profession:
+        return jsonify({"erro": "Faltando dados obrigatórios"}), 400
+
+    new_npc = {
+        "name": name,
+        "age": age,
+        "gender": gender,
+        "position": position,
+        "profession": profession
+    }
+
+    npc.insert_one(new_npc)
+    return jsonify({"mensagem": "npc criada com sucesso!"}), 201
+
+@app.route('/api/update/npc', methods=['PUT'])
+def update_npc():
+    data = request.json
+    name = data.get("name")
+    new_name = data.get("new_name")
+    new_age = data.get("new_age")
+    new_gender = data.get("new_gender")
+    new_position = data.get("new_position")
+    new_profession = data.get("new_profession")
+
+    if not name or not new_name or not new_age or not new_gender or not new_position or not new_profession:
+        return jsonify({"erro": "Faltando dados obrigatórios"}), 400
+    
+    update_fields = {}
+    if new_name:
+        update_fields["name"] = new_name
+    if new_age:
+        update_fields["age"] = new_age
+    if new_gender:
+        update_fields["gender"] = new_gender
+    if new_position:
+        update_fields["position"] = new_position
+    if new_profession:
+        update_fields["profession"] = new_profession
+
+    if not update_fields:
+        return jsonify({"message": "Nada para atualizar"}), 400
+
+    resultado = npc.update_one(
+        {"name": name},
+        {"$set": update_fields}
+    )
+
+    if resultado.modified_count == 1:
+        return jsonify({"mensagem": "Dados do npc atualizados com sucesso!"})
+    else:
+        return jsonify({"mensagem": "Nenhum npc atualizado. Verifique o username."})
+
+@app.route('/api/delete/npc', methods=['DELETE'])
+def delete_npc():
+    data = request.json
+    name = data.get("name")
+
+    if not name:
+        return jsonify({"message": "info error"})
+    
+    resultado = npc.delete_one(
+        {"name": name}
+    )
+
+    if resultado.deleted_count == 1:
+        return jsonify({"mensagem": "npc deletado com sucesso!"})
+    else:
+        return jsonify({"mensagem": "Nenhum npc deletado. Verifique os campos."})
 
 if __name__ == '__main__':
     app.run(debug=True)
