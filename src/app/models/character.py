3ae8f9b50d544bdbd7ff3478bbe2f01209b4763e
Dodas <ph005009004@gmail.com>
2025-05-23 19:26:51 -0300

    migrated to FastAPI
    implemented database

--- src/app/models/character.py
+++
@@ -1,8 +0,0 @@
-from beanie import Document
-
-class Character(Document):
-    name: str
-    age: int
-    gender: str
-    profession: str
-    role: str
--- src/app/models/user_progress.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-class UserProgress(BaseModel):
-    trust: int
-    number_of_cases_won: int
-    number_of_lost_cases: int
